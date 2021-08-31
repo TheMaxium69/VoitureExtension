5ac57b4de2b959a6643eb0bc8cbdc53a87fb90f4
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-08-31 16:19:11 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: VoitureExtension
+Plugin URI : https://github.com/TheMaxium69/VoitureExtension
+Author: Maxime Tournier
+Author URI: https://tyrolium.fr
+Description: Extension WordPress, Extension de Voiture
+Version: 1.0-BETA
+*/
